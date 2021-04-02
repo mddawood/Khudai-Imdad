8cce9776723a92b10737e2185f95bb548e79dd1c
Dawood <md1dawood@gmail.com>
2021-04-02 18:27:19 +0530

    alert added

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
     */
+    alert("The page is under construction!")
+
     (function ($) {
     "use strict"; // Start of use strict
 
